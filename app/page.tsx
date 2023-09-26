0fd3fa54baefd3edd9a4c6b45cefe4dbb99484d1
Darius Seals <tristarmediagroupinc@gmail.com>
2023-09-25 22:27:52 -0400

    added components dir, updated global styles, added Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero/Hero";
+
 export default function Home() {
-  return <main className="overflow-hidden">Hello World</main>;
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  );
 }
