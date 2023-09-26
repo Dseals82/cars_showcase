e890db00768f870fad683c243f005e25cef6bd93
Darius Seals <tristarmediagroupinc@gmail.com>
2023-09-25 22:58:52 -0400

    added CustomButton component and boiler plate code

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
+import CustomButton from "../CustomButton/CustomButton";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, or rent a car &mdash; quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental expereicne with our effortless booking
+          precess
+        </p>
+        <CustomButton />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
