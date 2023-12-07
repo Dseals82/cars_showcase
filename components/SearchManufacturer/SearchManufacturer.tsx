7b0a6f9fdb21ae9c69bb2c2670c30c84852ecd15
Darius Seals <tristarmediagroupinc@gmail.com>
2023-12-06 23:36:34 -0500

    update input error color to red

--- components/SearchManufacturer/SearchManufacturer.tsx
+++ components/SearchManufacturer/SearchManufacturer.tsx
@@ -55,7 +55,7 @@ const SearchManufacturer = ({
           >
             <Combobox.Options>
               {filteredManufacturers.length === 0 && query !== "" ? (
-                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                <div className="relative cursor-default select-none px-4 py-2 text-red-600">
                   Nothing found.
                 </div>
               ) : (
