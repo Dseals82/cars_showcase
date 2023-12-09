bcf29d934f0db2cdff78a017f7e20a4ab0ef7700
Darius Seals <tristarmediagroupinc@gmail.com>
2023-12-09 09:34:33 -0500

    created functionality for SearchMore button

--- components/ShowMore/ShowMore.tsx
+++ components/ShowMore/ShowMore.tsx
@@ -2,9 +2,30 @@
 
 import { useRouter } from "next/navigation";
 import { ShowMoreProps } from "./types";
+import CustomButton from "../CustomButton/CustomButton";
+import { updateSearchParams } from "@/utils";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-  return <div></div>;
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+    router.push(newPathName);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMore;
